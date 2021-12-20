03e8d06789f098d8fffd6b642b7b526ae3fe2dc1
Владислав Бошнаков <vboshnakov02@gmail.com>
2021-12-20 17:15:54 +0200

    Done user secrets

--- ShopCore.Mvc/Startup.cs
+++ ShopCore.Mvc/Startup.cs
@@ -38,7 +38,7 @@ namespace ShopCore
                     .AddCookie();
 
             services.AddDbContext<ShopDBContext>(options =>
-            options.UseSqlServer("Data Source=DESKTOP-MPTUQQD;Initial Catalog=ShopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;MultipleActiveResultSets=true"));
+            options.UseSqlServer("DefaultConnectionString"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
