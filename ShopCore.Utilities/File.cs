0df280ec8e586fb5e54d7c30f10b4dc879c15526
Владислав Бошнаков <vboshnakov02@gmail.com>
2022-01-14 15:07:48 +0200

    Back to original

--- ShopCore.Utilities/File.cs
+++ ShopCore.Utilities/File.cs
@@ -7,15 +7,8 @@ using static ShopCore.Services.Settings.MailSettings;
 
 namespace ShopCore.Utilities
 {
-        
     public class File
-        {
-
-        private readonly MailSettings mailSettings;
-        public File(IOptions<MailSettings> mailSettings)
-        {
-            this.mailSettings = mailSettings.Value;
-        }
+    {
         public static string GetFileFullName(IFormFile files)
         {
             var fileName = Path.GetFileName(files.FileName);
