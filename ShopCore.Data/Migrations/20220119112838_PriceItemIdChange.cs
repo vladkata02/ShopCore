1ee7998a15b035eb6d573e66efc61d5b587134c2
Владислав Бошнаков <vboshnakov02@gmail.com>
2022-01-19 14:03:06 +0200

    New migration and refactor PriceRepo

---
+++ ShopCore.Data/Migrations/20220119112838_PriceItemIdChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopCore.Data.Migrations
+{
+    public partial class PriceItemIdChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Prices",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemId",
+                table: "Prices",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
